b7f148fa89faf30f04b6e9da2cc0995594566a27
+ <+>
2022-08-17 12:59:08 -0400

    Done

--- index.js
+++ index.js
@@ -7,18 +7,19 @@ const multiply=num1*num2;//62
 let random=2
 let integer=true;
 
+Math.floor(Math.random()*100)+1;//increment is between 1 to 100
+
 //mod 4
 let mod=4
 let num3=10
 let num4=6
+num3%num4;//Result is 4
 
 //Maximum 20
 let max=20;
 (5,16,8,20);
 
-//math.max(5,16,8,20);
+math.max(5,16,8,20);
 
 
-
-Math.floor(math.random()*10+1);
 // Write your code here
