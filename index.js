3ce378b23d2f36c5f7a56042ddf85826a0e822d3
+ <+>
2022-08-17 12:57:53 -0400

    Done

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
+//*Gives 62
+let num1=31;
+let num2=2;
+const multiply=num1*num2;//62
 
+//Greater than 0
+let random=2
+let integer=true;
+
+//mod 4
+let mod=4
+let num3=10
+let num4=6
+
+//Maximum 20
+let max=20;
+(5,16,8,20);
+
+//math.max(5,16,8,20);
+
+
+
+Math.floor(math.random()*10+1);
 // Write your code here
